2966f445dbc2fa433697895931014e84c2efd9e7
Zubayraev <zubayraevzaurbek@gmail.com>
2025-05-19 21:32:17 +0200

    Billett endringer zaurbek

--- src/main/java/org/example/kino/Repository/BillettRepository.java
+++ src/main/java/org/example/kino/Repository/BillettRepository.java
@@ -3,11 +3,5 @@ package org.example.kino.Repository;
 import org.example.kino.Model.Billett;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface BillettRepository extends JpaRepository<Billett, Integer> {
-
-    List<Billett> findByErBetalt(boolean erBetalt);
-
-
+public interface BillettRepository extends JpaRepository<Billett, String> {
 }
