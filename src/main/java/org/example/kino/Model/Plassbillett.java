9587aaf2bb7873f0f6a2c17df11849a0ce8b0607
Sarmad <sarmad04@outlook.com>
2025-05-19 19:40:23 +0200

    sarmad

--- src/main/java/org/example/kino/Model/Plassbillett.java
+++ src/main/java/org/example/kino/Model/Plassbillett.java
@@ -1,4 +1,105 @@
 package org.example.kino.Model;
 
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tblPlassbillett")
+@IdClass(Plassbillett.sammensattPlassBillettId.class)
 public class Plassbillett {
+
+    @Id
+    @Column(name = "pb_billettkode", nullable = false, length = 6)
+    private String billettkode;
+
+    @Id
+    @Column(name = "pb_radnr")
+    private int radNr;
+
+    @Id
+    @Column(name = "pb_setenr")
+    private int seteNr;
+
+    @Id
+    @Column(name = "pb_kinosalnr")
+    private int kinosalNr;
+
+    // Constructors
+    public Plassbillett() {}
+
+    public Plassbillett(String billettkode, int radNr, int seteNr, int kinosalNr) {
+        this.billettkode = billettkode;
+        this.radNr = radNr;
+        this.seteNr = seteNr;
+        this.kinosalNr = kinosalNr;
+    }
+
+    // Getters and setters
+
+    public String getBillettkode() {
+        return billettkode;
+    }
+
+    public void setBillettkode(String billettkode) {
+        this.billettkode = billettkode;
+    }
+
+    public int getRadNr() {
+        return radNr;
+    }
+
+    public void setRadNr(int radNr) {
+        this.radNr = radNr;
+    }
+
+    public int getSeteNr() {
+        return seteNr;
+    }
+
+    public void setSeteNr(int seteNr) {
+        this.seteNr = seteNr;
+    }
+
+    public int getKinosalNr() {
+        return kinosalNr;
+    }
+
+    public void setKinosalNr(int kinosalNr) {
+        this.kinosalNr = kinosalNr;
+    }
+
+    // ID Class
+    public static class sammensattPlassBillettId implements Serializable {
+        private String billettkode;
+        private int radNr;
+        private int seteNr;
+        private int kinosalNr;
+
+        public sammensattPlassBillettId() {}
+
+        public sammensattPlassBillettId(String billettkode, int radNr, int seteNr, int kinosalNr) {
+            this.billettkode = billettkode;
+            this.radNr = radNr;
+            this.seteNr = seteNr;
+            this.kinosalNr = kinosalNr;
+        }
+
+        // equals and hashCode (viktig for sammensatte nøkler)
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof sammensattPlassBillettId that)) return false;
+            return radNr == that.radNr &&
+                    seteNr == that.seteNr &&
+                    kinosalNr == that.kinosalNr &&
+                    billettkode.equals(that.billettkode);
+        }
+
+        @Override
+        public int hashCode() {
+            return billettkode.hashCode() + radNr + seteNr + kinosalNr;
+        }
+    }
 }
+
+
