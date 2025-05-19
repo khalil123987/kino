2c570c1e59fdec9089e604c12faddfd4390f19eb
Sarmad <sarmad04@outlook.com>
2025-05-19 21:36:08 +0200

    Snake

---
+++ src/main/java/org/example/kino/Service/PlassBillettService.java
@@ -0,0 +1,32 @@
+package org.example.kino.Service;
+
+import org.example.kino.Model.Plassbillett;
+import org.example.kino.Repository.PlassBillettRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PlassBillettService {
+
+    @Autowired
+    private PlassBillettRepository plassBillettRepository;
+
+    public List<Plassbillett> finnAllePlassBilletter() {
+        return plassBillettRepository.findAll();
+    }
+
+    public Optional<Plassbillett> finnMedId(Plassbillett.SammensattPlassBillettId id) {
+        return plassBillettRepository.findById(id);
+    }
+
+    public Plassbillett lagrePlassBillett(Plassbillett plassBillett) {
+        return plassBillettRepository.save(plassBillett);
+    }
+
+    public void slettPlassBillett(Plassbillett.SammensattPlassBillettId id) {
+        plassBillettRepository.deleteById(id);
+    }
+}
