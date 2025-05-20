c69e8cf41376b1026b5485787730976af6a64076
Sarmad <sarmad04@outlook.com>
2025-05-20 13:17:25 +0200

    sånn

--- src/main/java/org/example/kino/Repository/VisningRepository.java
+++ src/main/java/org/example/kino/Repository/VisningRepository.java
@@ -1,10 +1,9 @@
 package org.example.kino.Repository;
 
-
 import org.example.kino.Model.Visning;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VisningRepository extends JpaRepository<Visning, Long> {
+public interface VisningRepository extends JpaRepository<Visning, Integer> {
     // Ekstra spørringer kan defineres her ved behov
 }
 
